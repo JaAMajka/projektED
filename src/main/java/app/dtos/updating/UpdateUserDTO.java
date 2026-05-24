8d6c6c6d6cb068b33733f01e5a9ec45e98b2473b
Jakub Jasiński <xkakashix135@gmail.com>
2026-05-24 17:23:42 +0200

    Update user DTO  - change password logic has been modified

--- src/main/java/app/dtos/updating/UpdateUserDTO.java
+++ src/main/java/app/dtos/updating/UpdateUserDTO.java
@@ -1,11 +1,12 @@
 package app.dtos.updating;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Pattern;
 
 public record UpdateUserDTO(
         @NotEmpty String name,
-        @NotEmpty String email,
-        @NotEmpty @Length(min = 8) String password,
+        @NotEmpty @Email String email,
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Password must contain at least 8 characters, one uppercase, one lowercase, one digit and one special character") String password,
         Boolean isStudent, Boolean prefersCardPayment) {
 }
