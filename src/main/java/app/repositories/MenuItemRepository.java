af5cce280ebcea1a66711c5ab7419ec2074a16e2
Jakub Jasiński <xkakashix135@gmail.com>
2026-06-07 23:12:59 +0200

    query fix

--- src/main/java/app/repositories/MenuItemRepository.java
+++ src/main/java/app/repositories/MenuItemRepository.java
@@ -15,8 +15,9 @@ public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
     Optional<MenuItem> findByCafeAndName(Cafe cafe, String name);
     List<MenuItem> findAllByCafeId(Long cafeId);
-    @Query("SELECT AVG(m.price), m.type FROM MenuItem m WHERE m.cafe.id = :cafeId GROUP BY m.type")
+    @Query("SELECT AVG(m.price), m.type FROM MenuItem m WHERE m.cafe.id = :cafeId AND m.isAppendage = false GROUP BY m.type")
     List<AvgPriceProjection> findAvgPricePerType(Long cafeId);
-    @Query("SELECT EXISTS( SELECT 1 FROM MenuItem m WHERE m.isIced = true AND m.cafe.id = :cafeId)")
+    @Query("SELECT EXISTS( SELECT 1 FROM MenuItem m WHERE m.isIced = true AND m.isAppendage = false AND m.cafe.id = :cafeId)")
     Boolean checkIfCafeHasIcedItems(Long cafeId);
+
 }
