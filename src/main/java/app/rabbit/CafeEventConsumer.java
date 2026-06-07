29ac64f7206e59172b1be8429ca82d460139bef4
Jakub Jasiński <xkakashix135@gmail.com>
2026-06-07 18:50:22 +0200

    Averages update has been implemented by creating necessary methods in service and adjusting it to cafe event consumer class

--- src/main/java/app/rabbit/CafeEventConsumer.java
+++ src/main/java/app/rabbit/CafeEventConsumer.java
@@ -1,4 +1,21 @@
 package app.rabbit;
+import app.repositories.MenuItemRepository;
+import app.services.CafeReadModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
 public class CafeEventConsumer {
+    private final MenuItemRepository menuItemRepository;
+    private final CafeReadModelService cafeReadModelService;
+
+    @RabbitListener(queues = RabbitMQConfig.MENU_ITEM_QUEUE)
+    public void recalculatePriceAverages(Long cafeId){
+        cafeReadModelService.recalculateAverages(cafeId, menuItemRepository.findAvgPricePerType(cafeId));
+    }
+
+
+
 }
--- src/main/java/app/services/CafeReadModelService.java
+++ src/main/java/app/services/CafeReadModelService.java
@@ -1,4 +1,37 @@
 package app.services;
 
+import app.BeverageType;
+import app.models.CafeReadModel;
+import app.projections.AvgPriceProjection;
+import app.repositories.CafeReadModelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class CafeReadModelService {
+    private final CafeReadModelRepository cafeReadModelRepository;
+
+    public void recalculateAverages(Long cafeId, List<AvgPriceProjection> averages){
+        CafeReadModel cafeReadModel = cafeReadModelRepository.findByCafeId(cafeId).orElseThrow();
+        Map<BeverageType, BigDecimal> avgPrices = averages.stream()
+                .collect(Collectors.toMap(
+                        AvgPriceProjection::getType,
+                        AvgPriceProjection::getAvgPrice
+                ));
+        cafeReadModel.setAvgBaggedTeaPrice(avgPrices.getOrDefault(BeverageType.BAGGED_TEA, null));
+        cafeReadModel.setAvgCoffeePrice(avgPrices.getOrDefault(BeverageType.PURE_COFFEE, null));
+        cafeReadModel.setAvgMatchaPrice(avgPrices.getOrDefault(BeverageType.MATCHA, null));
+        cafeReadModel.setAvgFruityDrinkPrice(avgPrices.getOrDefault(BeverageType.FRUITY_DRINK, null));
+        cafeReadModel.setAvgLeafTeaPrice(avgPrices.getOrDefault(BeverageType.LEAF_TEA, null));
+        cafeReadModel.setAvgWhiteCoffeePrice(avgPrices.getOrDefault(BeverageType.COFFEE_DRINKS, null));
+        cafeReadModel.setAvgOtherPrice(avgPrices.getOrDefault(BeverageType.OTHER, null));
+        cafeReadModelRepository.save(cafeReadModel);
+    }
+
 }
