e1a376e302a1c565dbd0574e33bfc17d795ee132
Jakub Jasiński <xkakashix135@gmail.com>
2026-06-07 18:49:00 +0200

    Method that allows to find read model by cafe ID has been implemented

--- src/main/java/app/repositories/CafeReadModelRepository.java
+++ src/main/java/app/repositories/CafeReadModelRepository.java
@@ -5,5 +5,8 @@ import app.models.CafeReadModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface CafeReadModelRepository extends JpaRepository<CafeReadModel, Long>, JpaSpecificationExecutor<CafeReadModel> {
+    Optional<CafeReadModel> findByCafeId(Long id);
 }
