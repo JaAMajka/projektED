75c3b4acd399f6162f2e8a3e242567d1ecfdd151
Jakub Jasiński <xkakashix135@gmail.com>
2026-06-09 15:15:56 +0200

    Projection needed to handle response from rate repository has been implemented.

--- src/main/java/app/projections/GlobalStatsRateProjection.java
+++ src/main/java/app/projections/GlobalStatsRateProjection.java
@@ -1,4 +1,13 @@
 package app.projections;
 
+import java.math.BigDecimal;
+
 public interface GlobalStatsRateProjection {
+    Long getCafeId();
+    BigDecimal getAvgAtmosphereScore();
+    BigDecimal getAvgBeverageScore();
+    BigDecimal getAvgServiceScore();
+    BigDecimal getStdDevAtmosphere();
+    BigDecimal getStdDevBeverage();
+    BigDecimal getStdDevService();
 }
